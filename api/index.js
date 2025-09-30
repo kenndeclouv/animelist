9d453807bcd6b6adafd4e012f0461c3d4f5ece82
kenndeclouv <zuuliziluuz@gmail.com>
2025-09-30 14:18:08 +0700

    test

--- api/index.js
+++ api/index.js
@@ -1,4 +1,21 @@
 // api/index.js
-export default function handler(req, res) {
-  res.status(200).send("Widget is ready!");
+import Anilist from "anilist-node";
+const anilist = new Anilist();
+
+export default async function handler(req, res) {
+  try {
+    // Ganti 'NekomataOkaayu' dengan username target
+    const username = "kenndeclouv";
+    const user = await anilist.user.all(username);
+    const userId = user.id;
+
+    // Ambil 5 aktivitas terakhir (nonton, baca, dll)
+    const activities = await anilist.user.getRecentActivity(userId, 1, 5);
+
+    // Kirim datanya sebagai JSON untuk tes
+    res.status(200).json(activities);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching data" });
+  }
 }
